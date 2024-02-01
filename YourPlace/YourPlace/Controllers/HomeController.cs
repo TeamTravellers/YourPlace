17ae67a0227521bba6737caf7bee760099223dd5
Elit0o <eli8.2013@gmail.com>
2024-02-01 21:06:43 +0200

    REDIRECTIONNNNN!!!!!!!!!!!!!

--- YourPlace/YourPlace/Controllers/HomeController.cs
+++ YourPlace/YourPlace/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace YourPlace.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private const string homePage = "Index";
+        private const string english = "~/Views/English/Index.cshtml";
+        private const string bulgarian = "~/Views/Bulgarian/Index.cshtml";
+        private const string ImagesPath = "/Images/ProductImages";
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -22,6 +27,18 @@ namespace YourPlace.Controllers
         {
             return View();
         }
+
+        public IActionResult English()
+        {
+            return View(english);
+        }
+
+        public IActionResult Bulgarian()
+        {
+            return View(bulgarian);
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
