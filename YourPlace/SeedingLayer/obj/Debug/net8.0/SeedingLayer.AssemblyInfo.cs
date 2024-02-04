402f723927a5f8e2fd6d49c374ee5c975fe8ae80
Elit0o <eli8.2013@gmail.com>
2024-02-04 16:59:05 +0200

    offers page

--- YourPlace/SeedingLayer/obj/Debug/net8.0/SeedingLayer.AssemblyInfo.cs
+++ YourPlace/SeedingLayer/obj/Debug/net8.0/SeedingLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeedingLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afab95fb664af3229d066494d3d652e9f096234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6893fe9d5d9df6e821be1551503bca32945fac7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeedingLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeedingLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- YourPlace/YourPlace/obj/Debug/net8.0/YourPlace.AssemblyInfo.cs
+++ YourPlace/YourPlace/obj/Debug/net8.0/YourPlace.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourPlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afab95fb664af3229d066494d3d652e9f096234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6893fe9d5d9df6e821be1551503bca32945fac7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourPlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourPlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
