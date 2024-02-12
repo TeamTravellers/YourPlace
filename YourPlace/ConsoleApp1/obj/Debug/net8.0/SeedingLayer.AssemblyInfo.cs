cc2e18ab6ff68de6b41061c428026c50726fdc88
Elit0o <eli8.2013@gmail.com>
2024-02-12 08:02:46 +0200

    not properly working filters button
    TO DO: add google maps link to the other button

--- YourPlace/ConsoleApp1/obj/Debug/net8.0/SeedingLayer.AssemblyInfo.cs
+++ YourPlace/ConsoleApp1/obj/Debug/net8.0/SeedingLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeedingLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3c327927b93d687e58f7482bc05c78c3bec2fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a22b2e65264c68f8798a7623629221fc0f534d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeedingLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeedingLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- YourPlace/YourPlace.Core/obj/Debug/net8.0/YourPlace.Core.AssemblyInfo.cs
+++ YourPlace/YourPlace.Core/obj/Debug/net8.0/YourPlace.Core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourPlace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3c327927b93d687e58f7482bc05c78c3bec2fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a22b2e65264c68f8798a7623629221fc0f534d")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourPlace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourPlace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
