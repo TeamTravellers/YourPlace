21987894ecf0614a2925701af13b93cb3e8e5d36
Aneliya05 <89069514+Aneliya05@users.noreply.github.com>
2024-02-13 12:32:16 +0200

    paragraphs done

--- YourPlace/YourPlace/obj/Debug/net8.0/YourPlace.AssemblyInfo.cs
+++ YourPlace/YourPlace/obj/Debug/net8.0/YourPlace.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourPlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecf5c46665ff93ce9de3c3a6c8f299b7c988afd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403a52fdc9835d45b418ad4297be5c11c173020f")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourPlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourPlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
