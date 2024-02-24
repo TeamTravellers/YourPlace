7cb7e3caa96c30dd59bf27b667cae99bae168047
Elit0o <eli8.2013@gmail.com>
2024-02-24 09:31:43 +0200

    .

--- YourPlace/YourPlace.Infrastructure/obj/Debug/net8.0/YourPlace.Infrastructure.AssemblyInfo.cs
+++ YourPlace/YourPlace.Infrastructure/obj/Debug/net8.0/YourPlace.Infrastructure.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourPlace.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235a8fd76bebbf10aff8a9373f4798fcd829f788")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourPlace.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourPlace.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
