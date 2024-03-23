5fa09617249b2d87ebecd733cffa725be9940af2
Aneliya05 <89069514+Aneliya05@users.noreply.github.com>
2024-03-23 09:48:59 +0200

    ot eli :D`

--- YourPlace/YourPlace.Core/obj/Debug/net8.0/YourPlace.Core.AssemblyInfo.cs
+++ YourPlace/YourPlace.Core/obj/Debug/net8.0/YourPlace.Core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourPlace.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f19185fcb4a40c973d652c3c32a0cffd1a05e03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9f8c05b42806a9e86957c32045b9bd7d4af299")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourPlace.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourPlace.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
